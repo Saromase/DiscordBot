168710a70560c8c59720ac6c53663e356a4409c4
Saromase <seite.romain@hotmail.fr>
2022-01-01 00:39:27 +0100

    Refacto economie

--- commands/economy/bank.js
+++ commands/economy/bank.js
@@ -7,31 +7,124 @@ const {
 } = require('../../collectionInit');
 
 const {
+	Accounts,
+} = require('../../dbObjects');
+
+const {
 	configChannel,
+	roles,
 } = require('../../config.json');
+
+const { USER_TYPE, ROLE_TYPE } = require('../../utils/TargetConstant');
 
 module.exports = {
 	authorizedChan : configChannel.bank,
 	data: new SlashCommandBuilder()
 		.setName('bank')
 		.setDescription('Retourne le compte de l\'utilisateur!')
-		.addRoleOption(option => option.setName('role').setDescription('Role')),
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('create')
+				.setDescription('Créer un compte bancaire (Gouvernement uniquement)')
+				.addMentionableOption(options => options.setDescription('Utilisateur ou role').setName('mention').setRequired(true)))
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('check')
+				.setDescription('Vérifie un compte bancaire.')
+				.addRoleOption(option => option.setName('role').setDescription('Role')))
+		.addSubcommand(subcommand =>
+			subcommand
+				.setName('delete')
+				.setDescription('Supprime un compte bancaire. (Gouvernement uniquement)')
+				.addMentionableOption(options => options.setDescription('Utilisateur ou role').setName('mention').setRequired(true))),
 	async execute(interaction) {
-		const role = interaction.options.getRole('role');
-		const target = role ?? interaction.user;
-
-		const account = Account.get(target.id);
-
-		if (account) {
-			const embed = require('../../embedded/account')(account.balance);
-			await interaction.reply({
-				embeds: [embed],
-				ephemeral : true,
-			});
-			return;
+		let target;
+		const subCommand = interaction.options.getSubcommand();
+		const userRoles = interaction.guild.members.cache.get(interaction.user.id).roles;
+		const adminOrModerator = userRoles.cache.get(roles.admin) || userRoles.cache.get(roles.moderator);
+		if (subCommand === 'check') {
+			const role = interaction.options.getRole('role');
+			if (role) {
+				const canCheck = userRoles.cache.get(role.id) || adminOrModerator;
+				if (!canCheck) {
+					await interaction.reply({ content : `Vous ne pouvez pas consulter le compte de ${role}`, ephemeral : true });
+					return;
+				}
+			}
+			target = role ?? interaction.user;
+		}
+		else {
+			target = interaction.options.getMentionable('mention');
 		}
 
-		await interaction.reply({ content : `Aucun compte associé à ${target}`, ephemeral : true });
+		const account = Account.get(target.id);
+		const canCreate = userRoles.cache.get(roles.government) || adminOrModerator;
+
+		switch (subCommand) {
+		case 'check':
+			if (account) {
+				const embed = require('../../embedded/account')(account.balance);
+				await interaction.reply({
+					embeds: [embed],
+					ephemeral : true,
+				});
+			}
+			else {
+				await interaction.reply({ content : `Aucun compte associé à ${target}`, ephemeral : true });
+			}
+			break;
+		case 'create':
+			if (!canCreate) {
+				await interaction.reply({
+					content : `Vous ne pouvez pas créer de compte car vous n'êtes pas membre du ${interaction.guild.roles.cache.get(roles.government)}`,
+					ephemeral : true,
+				});
+			}
+			else if (!account) {
+				const type = target.user ? USER_TYPE : ROLE_TYPE;
+				const newAccount = await Accounts.create({
+					target_id : target.id,
+					target_type : type,
+					balance : 0,
+				});
+				Account.set(target.id, newAccount);
+				await interaction.reply({
+					content : `Compte crée pour ${target}`,
+					ephemeral : true,
+				});
+			}
+			else {
+				await interaction.reply({
+					content : `Un compte existe déjà pour ${target}. Utiliser /bank @${target.name} pour consulter le solde`,
+					ephemeral : true,
+				});
+			}
+			break;
+		case 'delete':
+			if (adminOrModerator) {
+				await Accounts.delete({
+					where : {
+						target_id : target.id,
+					},
+				});
+				Account.delete(target.id);
+				await interaction.reply({
+					content : `Compte supprimer pour ${target}`,
+					ephemeral : true,
+				});
+				await interaction.reply({
+					content : `Vous ne pouvez pas créer de compte car vous n'êtes pas membre du ${interaction.guild.roles.cache.get(roles.government)}`,
+					ephemeral : true,
+				});
+			}
+			else {
+				await interaction.reply({
+					content : `Vous ne pouvez pas supprimer de compte car vous n'êtes pas membre du ${interaction.guild.roles.cache.get(roles.government)}`,
+					ephemeral : true,
+				});
+			}
+			break;
+		}
 		return;
 	},
 };
--- commands/economy/pay.js
+++ commands/economy/pay.js
@@ -10,33 +10,35 @@ const {
 	Accounts,
 } = require('../../dbObjects');
 
+const {
+	roles,
+} = require('../../config.json');
+
 
 module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('give-money')
-		.setDescription('Replies the bank of user!')
-		.addMentionableOption(options => options.setDescription('Personnes ou entité à payer').setName('mention').setRequired(true))
-		.addIntegerOption(options => options.setDescription('Montant à payer').setName('amount').setRequired(true)),
+		.setName('pay')
+		.setDescription('Transfert un montant vers un autre compte (Attention, cette attention est irréversible')
+		.addIntegerOption(options => options.setDescription('Montant à payer').setName('amount').setRequired(true))
+		.addMentionableOption(options => options.setDescription('Personnes ou Commerce à payer').setName('mention').setRequired(true)),
 	async execute(interaction) {
 		const mention = interaction.options.getMentionable('mention');
-		const amount = interaction.options.getInteger('amount');
-
-
-		if (amount < 0) {
-			interaction.reply({
-				content : 'Le montant doit être positif ! ',
-				ephemeral : true,
-			});
-		}
+		let amount = parseInt(interaction.options.getInteger('amount'), 10);
 		const user = interaction.user;
+		const userRoles = interaction.guild.members.cache.get(interaction.user.id).roles;
+		const isAdmin = userRoles.cache.get(roles.admin);
+
 		const userAccount = Account.get(user.id);
 		const mentionAccount = Account.get(mention.id);
+
+		const nickname = interaction.guild.members.cache.get(interaction.user.id).nickname;
 
 		if (!mentionAccount) {
 			interaction.reply({
 				content : 'Votre destinataire n\'as pas encore de compte, réessayer plus tard.',
 				ephemeral : true,
 			});
+			return;
 		}
 
 		if (!userAccount) {
@@ -44,8 +46,40 @@ module.exports = {
 				content : 'Vous n\'avez pas de compte, veuillez contacté la mairie.',
 				ephemeral : true,
 			});
+			return;
 		}
 
+		console.log();
+
+
+		if (amount < 0) {
+			if (isAdmin) {
+				amount = amount * -1;
+				Accounts.update({
+					balance : (mentionAccount.balance + amount),
+				}, {
+					where : {
+						target_id : mention.id,
+					},
+				});
+
+				interaction.reply({
+					content : `Le compte de ${mention} à était approvisionner de ${amount}`,
+					ephemeral : true,
+				});
+
+				if (mention.user) {
+					mention.send(`${nickname} vous à transféré ${amount}`);
+				}
+				return;
+			}
+			else {
+				interaction.reply({
+					content : 'Le montant doit être positif ! ',
+					ephemeral : true,
+				});
+			}
+		}
 		if (userAccount.balance >= amount) {
 			const promises = [
 				Accounts.update({
@@ -70,6 +104,12 @@ module.exports = {
 					content : `Vous avez payé ${amount} à ${mention}`,
 					ephemeral : true,
 				});
+				console.log(user);
+
+				if (mention.user) {
+					mention.send(`${nickname} vous à transféré ${amount}`);
+				}
+
 			}).catch(() => {
 				console.error(`Une erreur s'est produite lors de l'envoi d'argent entre ${mention} et ${user} d'un montant de ${amount}`);
 				interaction.reply({
